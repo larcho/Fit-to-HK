a93c10b6634e386a68c69ce06677b7a7b24aa12e
Lars Klassen <lars@teixido.co>
2019-01-21 14:25:26 -0300

    Initial Commit

---
+++ Fit to HK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fit to HK
+//
+//  Created by Lars Klassen on 1/21/19.
+//  Copyright © 2019 Lars Klassen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
